c2affb5eba8c2567719538ebd1880b9a02d28a4d
navnithb2001 <bharadwajnavnith5@gmail.com>
2023-02-22 18:08:41 +0530

    added cors filter

--- src/main/java/com/internship/usertracking/UserTrackingApplication.java
+++ src/main/java/com/internship/usertracking/UserTrackingApplication.java
@@ -2,6 +2,10 @@ package com.internship.usertracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class UserTrackingApplication {
@@ -9,5 +13,17 @@ public class UserTrackingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserTrackingApplication.class, args);
 	}
+
+    @Bean
+    CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 
 }
