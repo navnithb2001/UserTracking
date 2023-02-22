d58fd39b663af3ef359edd7fb6d373de2393aaf5
navnithb2001 <bharadwajnavnith5@gmail.com>
2023-02-22 18:38:04 +0530

    cors removed

--- src/main/java/com/internship/usertracking/UserTrackingApplication.java
+++ src/main/java/com/internship/usertracking/UserTrackingApplication.java
@@ -2,10 +2,6 @@ package com.internship.usertracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class UserTrackingApplication {
@@ -13,17 +9,4 @@ public class UserTrackingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserTrackingApplication.class, args);
 	}
-
-    @Bean
-    CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
 }
